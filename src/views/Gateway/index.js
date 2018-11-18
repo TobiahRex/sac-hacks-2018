8a57a5f133d9d71fd2c61d7444f3e133c05fd907
Tobiah Rex <tobiah.rex@gmail.com>
2018-11-17 22:59:27 -0800

    wrote "renderOptions" for gateway component.

--- src/views/Gateway/index.js
+++ src/views/Gateway/index.js
@@ -2,6 +2,7 @@ import React from 'react';
 import { connect } from 'react-redux';
 import apiActions from '../../redux/api';
 import gatewayActions from '../../redux/gateway';
+import Validation from 'react-validation';
 
 
 class Gateway extends React.Component {
@@ -14,46 +15,76 @@ class Gateway extends React.Component {
     };
   }
 
-  render() {
-    const {
-      originSchool,
-      destinationSchool
-    } = this.state;
+  renderOptions = options => options.map(({ name, code }) => (
+    <option key={Buffer.from(code, 'utf8').toString('base64')} value={name} dataCode={code}>
+      {name}
+    </option>));
 
-    return (
-      <pre>
-        Origin School: {originSchool} <br />
-        destination School: {destinationSchool}
-      </pre>
-    );
-  }
-}
+  
 
-export default connect(
-  ({ api: apiStatus }) => ({
-    apiStatus,
-  }),
-  dispatch => ({
-    redux: {
-      fetching: () => dispatch(apiActions.fetching()),
-      getOrigins: () => dispatch(gatewayActions.getOrigins()),
-      getDestinations: origin => dispatch(gatewayActions.getDestinations(origin)),
-      getCourses: (codes) => dispatch(gatewayActions.getCourses(codes.origin, codes.destination, codes.gpa)),
-    },
-  })
-)(Gateway);
+    render() {
+      const {
+        originSchool,
+        destinationSchool
+      } = this.state;
 
-/*
-  Control Flow:
+      return (
+        <div>
+          <div className="prefecture-section__container">
+            <label className="form__label" htmlFor="Prefecture">
+              <IntlMsg id="checkout.shipping-address.prefecture" />&nbsp;
+              <strong className="label__asterisk">*</strong>
+            </label>
+            <Validation.components.Select
+              errorClassName="form__error-blurb"
+              name="Origin School"
+              containerClassName="container__text-field"
+              validations={['required']}
+              value={originSchool}
+              onChange={this.handleOnChange}
+              >
+                <option value="">
+                  <IntlMsg id="checkout.shipping-address.prefecture.choose" />
+                </option>
+                {
+                  renderOptions(PrefectureConstants)
+                };
+              </Validation.components.Select>
+            </div>
+            <br/>
+            <pre id="result" >
+
+            </pre>
+          </div>
+        );
+      }
+    }
+
+    export default connect(
+      ({ api: apiStatus }) => ({
+        apiStatus,
+      }),
+      dispatch => ({
+        redux: {
+          fetching: () => dispatch(apiActions.fetching()),
+          getOrigins: () => dispatch(gatewayActions.getOrigins()),
+          getDestinations: origin => dispatch(gatewayActions.getDestinations(origin)),
+          getCourses: (codes) => dispatch(gatewayActions.getCourses(codes.origin, codes.destination, codes.gpa)),
+        },
+      })
+    )(Gateway);
+
+    /*
+    Control Flow:
     1. call getOrigins,
-      2. saga calls the api.
-      3. saga calls getOriginsSuccess
+    2. saga calls the api.
+    3. saga calls getOriginsSuccess
     4. call getDestinations,
-      5. saga calls the api.
-      6. saga calls getDestinationsSuccess
+    5. saga calls the api.
+    6. saga calls getDestinationsSuccess
     7. call getCourses
-      8. saga calls api.
-      9. saga calls getCoursesSuccess
+    8. saga calls api.
+    9. saga calls getCoursesSuccess
     10. redirect page.
 
-*/
+    */
